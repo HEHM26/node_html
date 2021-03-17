03aeb9d0502125db9cdf5f717bcc30615e32d963
HEHM26 <56568682+HEHM26@users.noreply.github.com>
2021-03-17 15:35:12 +0900

    Add files via upload

--- router/main.js
+++ router/main.js
@@ -7,10 +7,10 @@ module.exports = function(app) {
       }));
      
      app.get('/',function(req,res){
-        res.render('index.html')
+        res.render('./index.html')
      });
      app.get('/about',function(req,res){
-        res.render('about.html');
+        res.render('./about.html');
     });
      
     app.get('/listall', function (req, res) {
